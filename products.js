60fc3d1b96187fcac2cc0128a1cc67847ce7a017
PrintedSmilesCo <matthewgmills@icloud.com>
2026-02-02 18:17:20 -0600

    Update products.js

--- products.js
+++ products.js
@@ -14,4 +14,16 @@ window.PRODUCTS = [
     details: "Articulated tail with flexible joints. Head turns right and left.",
     tags: ["dragon", "flexi"]
   }
+
+  {
+    id: "colorful-coral", // use normal hyphen, not the long dash character
+    name: "Aquarium coral",
+    minAge: 3,
+    maxAge: 99,
+    inStock: true,
+    image: "images/colorful-coral.jpeg",
+    short: "Colorful and fun coral for your aquarium.",
+    details: "3-D Coral can be printed in various colors, shapes, and sizes",
+    tags: ["fish tank"]
+  }
 ];
