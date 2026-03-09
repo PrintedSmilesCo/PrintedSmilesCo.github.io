98c8cab79332c970aa7e4ea9bd419aa1fbc47a7d
PrintedSmilesCo <matthewgmills@icloud.com>
2026-03-08 19:34:16 -0500

    Update products.js

--- products.js
+++ products.js
@@ -3,17 +3,7 @@
 // Edit inStock true/false as you make/sell items.
 
 window.PRODUCTS = [
-  {
-    id: "winter-exclusive",
-    name: "Pine Cone Dragon - Winter Exclusive (100 Medallions)",
-    minAge: 3,
-    maxAge: 99,
-    inStock: true,
-    image: "images/winter-exclusive-2026.jpeg",
-    short: "Winter-exclusive flexi dragon.",
-    details: "Articulated tail with flexible joints. Head turns right and left.",
-    tags: ["dragon", "flexi"]
-  },
+  
   {
     id: "colorful-coral",
     name: "Aquarium Coral",
