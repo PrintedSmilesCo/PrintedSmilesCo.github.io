8d2fbad968e7ffd27919180f375e2690b1fedd39
PrintedSmilesCo <matthewgmills@icloud.com>
2026-04-25 09:51:05 -0500

    Update products.js

--- products.js
+++ products.js
@@ -14,7 +14,7 @@ window.PRODUCTS = [
   details: "3-D submarine, approximately 5 inches long, can be printed in various colors.",
   tags: ["fish tank", "aquarium", "decor"]
 },
-  {
+{
     id: "colorful-coral",
     name: "Aquarium Coral",
     minAge: 3,
@@ -24,6 +24,17 @@ window.PRODUCTS = [
     short: "Colorful and fun coral for your aquarium.",
     details: "3-D coral can be printed in various colors, shapes, and sizes.",
     tags: ["fish tank", "aquarium", "decor"]
-  }
+},
+{
+    id: "badsign",
+    name: "Bad Sign",
+    minAge: 3,
+    maxAge: 99,
+    inStock: true,
+    image: "images/BadSign.jpeg",
+    short: "A silly sign to brighten someone's day.",
+    details: "Approximately 4.5" x 4.5" and can be printed in various colors.",
+    tags: ["decor"]
+}
 ];
 console.log("Products loaded:", window.PRODUCTS);
